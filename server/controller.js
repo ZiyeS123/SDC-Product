310e12aa7c4b421d5be52ff975d4d1a01f6bdf90
SONG336 <songziye960123@gmail.com>
2021-12-01 17:35:37 -0500

    Install redis, Add redis caching layer on getProductList, getProductInfo

--- server/controller.js
+++ server/controller.js
@@ -1,27 +1,59 @@
 const { readProductList, readProductInfo, readRelatedProducts, readProductStyles } = require('./model.js')
+const redis = require('redis');
+
+//connect to redis client
+//const REDIS_PORT = process.env.PORT || 6379;
+const redisClient = redis.createClient();
+
+redisClient.on('err', (err) => {
+  console.log('Redis Client Error', err)
+});
+
 
 module.exports = {
   getProductList: function(req, res) {
     const page = req.query.page;
     const count = req.query.count;
-    readProductList(page, count)
-      .then((results) => {
-        res.send(results);
-      })
-      .catch((err) => {
-        console.log('ERROR IN get products/list', err);
-      })
+    const redisKey = `getProductList:${page}${count}`
+
+    redisClient.get(redisKey, (err, data) => {
+      if (err) throw err;
+      if (data !== null) {
+        res.send(JSON.parse(data))
+      } else {
+        readProductList(page, count)
+          .then((results) => {
+            redisClient.setex(redisKey, 3600, JSON.stringify(results));
+            res.send(results);
+          })
+          .catch((err) => {
+            console.log('ERROR IN get products/list', err);
+          })
+      }
+    })
   },
 
   getProductInfo: function(req, res) {
     const id = req.params.product_id;
-    readProductInfo(id)
-      .then((results) => {
-        res.send(results);
-      })
-      .catch((err) => {
-        console.log('ERROR IN get products/:product_id', err);
-      })
+    const redisKey = `getProductInfo:${id}`;
+
+    redisClient.get(redisKey, (err, data) => {
+      if (err) throw err;
+      if (data !== null) {
+        //console.log('🌈data already cached')
+        res.send(JSON.parse(data))
+      } else {
+        readProductInfo(id)
+          .then((results) => {
+            //console.log('🌈data not cached')
+            redisClient.setex(redisKey, 3600, JSON.stringify(results));
+            res.send(results);
+          })
+          .catch((err) => {
+            console.log('ERROR IN get products/:product_id', err);
+          })
+      }
+    })
   },
 
   getRelatedProducts: function(req, res) {
